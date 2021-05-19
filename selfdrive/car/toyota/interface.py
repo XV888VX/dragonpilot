335069247608f63e3f0c082bc4da1eb77def01eb
cgw1968-5779 <58063525+cgw1968-5779@users.noreply.github.com>
2021-05-20 07:53:45 +0800

    Update interface.py

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -252,13 +252,13 @@ class CarInterface(CarInterfaceBase):
             ret.lateralTuning.pid.kfBP = [0, 22, 40]
             ret.lateralTuning.pid.kpV = [0.6, 0.6, 0.3, 0.3] # power or torque
             ret.lateralTuning.pid.kiV = [0.185, 0.185, 0.02, 0.02] # trajectory variance
-            ret.lateralTuning.pid.kfV = [0.00007818594, 0.00007818594, 0.00004] # curvature
+            ret.lateralTuning.pid.kfV = [0.00031415926535897932384626433832795, 0.00031415926535897932384626433832795, 0.00004] # curvature
             #ret.lateralTuning.pid.kpBP = [0, 23.6, 23.61, 40]
             #ret.lateralTuning.pid.kiBP = [0, 23.6, 23.61, 40]
             #ret.lateralTuning.pid.kfBP = [0, 23.6, 40]
             #ret.lateralTuning.pid.kpV = [1.0, 1.0, 0.6, 0.6] # power or torque
             #ret.lateralTuning.pid.kiV = [0.87, 0.87, 0.03, 0.03] # trajectory variance
-            #ret.lateralTuning.pid.kfV = [0.000314159, 0.000314159, 0.000314159] # curvature
+            #ret.lateralTuning.pid.kfV = [0.00031415926535897932384626433832795, 0.00031415926535897932384626433832795, 0.00007818594] # curvature
             break
 
     elif candidate == CAR.RAV4H_TSS2:
